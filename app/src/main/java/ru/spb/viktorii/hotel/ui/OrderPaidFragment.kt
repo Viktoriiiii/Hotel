ed6bc232f866a07b6391c95aa9f55ec3f36e2101
Виктория Самсонова <white013rose@gmail.com>
2023-09-01 17:56:05 +0300

    Initial commit

--- app/src/main/java/ru/spb/viktorii/hotel/ui/OrderPaidFragment.kt
+++ app/src/main/java/ru/spb/viktorii/hotel/ui/OrderPaidFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ru.spb.viktorii.hotel.R
 import ru.spb.viktorii.hotel.databinding.FragmentOrderPaidBinding
 
 class OrderPaidFragment : Fragment() {
@@ -16,7 +15,7 @@ class OrderPaidFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentOrderPaidBinding.inflate(inflater, container, false)
         return binding.root
     }
